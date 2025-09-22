e749e518ff58ad31e097dcefd019ddb21fa945aa
“Harshi“ <harshita.mailme@gmail.com>
2025-09-22 12:18:57 +0200

    made fav card

--- src/ui.js
+++ src/ui.js
@@ -26,28 +26,30 @@ const createCardHTML = (movie) => {
 
 // # create html for favourite movies
 const createFavCardHTML = (movie) => {
-  return `<div class="flex bg-base-100 w-70 md:w-auto rounded-2xl shadow-sm pb-2">
-            <figure class="px-3 pt-3">
-              <img
-              class="w-20 h-15 rounded-xl"
-              src=${imageBaseUrl}${movie.poster_path}
-              alt=${movie.title}
-              class="rounded-xl"
-              />
-            </figure>
-            <div class="felx flex-col justify-between">
-              <h2 class="font-bold mt-1">${movie.title}</h2>
-              <div class="">
-                <button                
-                class="border border-purple-200 px-2 rounded-lg text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 mt-3 removeMovie"
-                data-movie-id='${movie.id}'
-                >
-                Remove from list
-              </button>
-            </div>
-          </div>
-        </div>`;
+  return `
+    <div class="card bg-base-100 w-60 shadow-sm hover:shadow-lg hover:scale-105 transition">
+      <figure class="px-3 pt-3">
+        <img
+          src="${imageBaseUrl}${movie.poster_path}"
+          alt="${movie.title}"
+          class="rounded-xl object-cover h-80 w-full"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title line-clamp-1">${movie.title}</h2>
+        <p class="text-sm text-gray-600 line-clamp-3">${movie.overview}</p>
+        <div class="card-actions">
+          <button class="btn btn-primary removeMovie" data-movie-id='${movie.id}'>
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
 };
+
+
+
 
 export { createFavCardHTML, createCardHTML };
 
